677451a72946ceb4b38c1122a90a309712ab812e
GitHub Action <action@github.com>
2024-02-21 03:20:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/zhaolin1998/giteecodeideaplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/zhaolin1998/giteecodeideaplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.zhaolin1998.giteecodeideaplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
